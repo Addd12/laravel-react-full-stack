4bb7600fe1f0d9062ebf2616d6c7f432c920ba01
Extramus-Dev <91184994+Extramus-Dev@users.noreply.github.com>
2024-05-30 23:04:38 +0200

    connected to the backend using axios and setup signup frontend

--- react/src/views/Signup.jsx
+++ react/src/views/Signup.jsx
@@ -1,20 +1,55 @@
 import { Link } from "react-router-dom";
+import {useRef} from "react";
+import axiosClient from "../../axios-client";
 
-export default function Signup(){
+export default function Signup() {
+    const nameRef = useRef();
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const passwordConfirmationRef = useRef();
+
+    const {setUser, setToken} = useStateContext()
 
     const onSubmit = (ev) => {
-        ev.preventDefault()
-    }
+        ev.preventDefault();
+        const payload = {
+            name: nameRef.current.value,
+            email: emailRef.current.value,
+            password: passwordRef.current.value,
+            password_confirmation: passwordConfirmationRef.current.value,
+        };
+        //console.log(payload)
+        //this is the request we make to the server
+        axiosClient.post('/signup', payload)
+        .then(({data}) => {
+            setUser(data.user)
+            setToken(data.token)
+        })
+        .catch(err => {
+            const response = err.response;
+            if (response && response.status === 422){
+                console.log(response.data.errors);
+            }
+        })
+    };
 
-    return(
+    return (
         <div className="login-signup-form animated fadeInDown">
             <div className="form">
                 <form onSubmit={onSubmit}>
                     <h1 className="title">Sign up for free</h1>
-                    <input type="text" placeholder="Full name" />
-                    <input type="email" placeholder="Email" />
-                    <input type="password" placeholder="Password" />
-                    <input type="password" placeholder="Password confirmation" />
+                    <input ref={nameRef} placeholder="Full name" />
+                    <input ref={emailRef} type="email" placeholder="Email" />
+                    <input
+                        ref={passwordRef}
+                        type="password"
+                        placeholder="Password"
+                    />
+                    <input
+                        ref={passwordConfirmationRef}
+                        type="password"
+                        placeholder="Password confirmation"
+                    />
                     <button className="btn btn-block">Signup</button>
                     <p className="message">
                         Already Registered? <Link to="/login">Sign in</Link>
@@ -22,5 +57,5 @@ export default function Signup(){
                 </form>
             </div>
         </div>
-    )
+    );
 }
