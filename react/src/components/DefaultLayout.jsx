0f9c5e58c22162c19393da22b847932326354bee
Extramus-Dev <91184994+Extramus-Dev@users.noreply.github.com>
2024-06-10 11:48:07 +0200

    implemented logout

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -1,15 +1,32 @@
 import { Link, Navigate, Outlet } from "react-router-dom";
 import { useStateContext } from "../contexts/ContextProvider";
+import { useEffect } from "react";
+import axiosClient from "../../axios-client";
 
 export default function DefaultLayout(){
-    const {user, token} = useStateContext()
+    const {user, token, setUser, setToken} = useStateContext()
     if(!token){
         return <Navigate to="/login" />
     }
 
     const onLogout = (ev) => {
         ev.preventDefault()
+
+        axiosClient.post('/logout') //whenever the logout happens
+        .then(() => { //then we do the following
+            setUser({}) //set user to be an empty object
+            setToken(null) //when the token is missing the user gets redirected to the login (should be defined as a const above)
+        })
     }
+
+    useEffect(() => {
+        axiosClient.get('/user')
+        //return the user info
+        .then(({data}) => { //destructure the response and get the data from there
+            setUser(data)
+        })
+    }, []) //without the square brackets it keeps sending requests
+
     return(
         <div id="defaultLayout">
             <aside>
